a5c20488e518df67ed55464c610cc40be2aec06b
Dawid-Drewicz_UoA <u08dd22@abdn.ac.uk>
2025-12-01 00:54:34 +0000

    Improve "enterItemFromKeyboard()" more checks and now skip redundant steps

--- Dawid_D/R1_DD.h
+++ Dawid_D/R1_DD.h
@@ -13,6 +13,7 @@
 class integer_itemWithLimits : public integer_item{
     protected: 
         int min_val, max_val; 
+        bool Lims_Set; 
 
     private:
     // privitising "generateRandomItemWithinlimits()" to prevent setting int val outisde limits
@@ -24,20 +25,22 @@ class integer_itemWithLimits : public integer_item{
             itemTypeName = "integer_itemWithLimits";
             min_val = -50; //  assumed defult vals
             max_val = 50; 
+            Lims_Set = false; // is false while vals defult 
         }
         ~integer_itemWithLimits(){;}
+        bool isLims_Set() const {return Lims_Set;}
 
         int GetMinVal()
         {
-            if(isEmpty())
-			cout << "*Warning*: GetMinVal() called on an empty object" << endl; 
+            if(!isLims_Set())
+			    cout << "*Note: Limits are there defult values" << endl; 
 		    return min_val;
         }
 
         int GetMaxVal()
         {
-            if(isEmpty())
-			cout << "*Warning*: GetMaxVal() called on an empty object" << endl; 
+            if(!isLims_Set())
+			    cout << "*Note: Limits are there defult values" << endl; 
 		    return max_val;
         }
 
@@ -48,11 +51,15 @@ class integer_itemWithLimits : public integer_item{
             if(isEmpty())
 			    cout << "Item is empty." << endl;
 		    else
-            {
 			    cout << "Item value is " << item_value << " . " << endl;
+                
+            if(isLims_Set())
+            {
                 cout << "Lower limit is " << min_val << "." << endl;
                 cout << "Upper limit is " << max_val << "." << endl;
             }
+            else
+                cout << "Limits are set to defult Values" << endl; 
         }
 
         // * Again like above or sepreate or mix have this but also 
@@ -63,7 +70,7 @@ class integer_itemWithLimits : public integer_item{
 			    cout << "Error in enterItemFromKeyboard: Item is locked" << endl;
 		    else
 		    {
-                bool Valid_int = false; 
+                bool Valid_int = false, new_Lims =false; 
                 char yesNo_in; 
 
                 // check if Limits need change 
@@ -77,17 +84,17 @@ class integer_itemWithLimits : public integer_item{
                     int tempL, tempU ; 
                     while(!valid_lim)
                     {
-                    cout << "Insert Lower Limit Value then hit enter."<< endl;
-                    cin >> tempL;
-                    cout << endl;
-                    cout << "Insert Upper Limit Value then hit enter."<< endl;
-                    cin >> tempU;
-                    cout << endl;
+                        cout << "Insert Lower Limit Value then hit enter."<< endl;
+                        cin >> tempL;
+                        cout << endl;
+                        cout << "Insert Upper Limit Value then hit enter."<< endl;
+                        cin >> tempU;
+                        cout << endl;
                     
-                    if(tempL != tempU)
-                        valid_lim = true; 
-                    else
-                        cout << "*Warning*: Limits cannot be the same Value." << endl; 
+                        if(tempL != tempU)
+                            valid_lim = true; 
+                        else
+                            cout << "*Warning*: Limits cannot be the same Value." << endl; 
                     }
 
                     if (tempL > tempU)  // make sure limits are correct way round
@@ -99,20 +106,33 @@ class integer_itemWithLimits : public integer_item{
 			        }
                     max_val = tempU; 
                     min_val = tempL; 
+                    new_Lims = true; 
+                    Lims_Set = true; 
+                }
+                yesNo_in = 'N'; // "restet" slection to reuse var  
+
+                if(new_Lims) // run only if new lims set w this call, Assume Min 1 must update .: if lims !set int must be & Q is redundant 
+                {
+                    cout << "Do you want to Enter New Integer ? type Y if yes then hit enter" << endl; 
+                    cin >> yesNo_in; 
                 }
 
-                while(!Valid_int)
+                if((yesNo_in=='Y')||(yesNo_in=='y')||(!new_Lims))
                 {
-			        cout << "Insert integer element then hit enter." << endl;
-			        cin >> item_value;
-			        cout << endl;
+                    while(!Valid_int)
+                    {
+			            cout << "Insert integer element then hit enter." << endl;
+			            cin >> item_value;
+			            cout << endl;
 			        
-                    if((item_value<=max_val)&&(item_value>=min_val))
-                        Valid_int = true; 
-                    else
-                        cout << "*Warning*: integer must fall within set limits" << endl << endl;    
+                        if((item_value<=max_val)&&(item_value>=min_val))
+                            Valid_int = true; 
+                        else
+                            cout << "*Warning*: integer must fall within set limits" << endl << endl;    
+                    }
+                    empty = false;
                 }
-                empty = false;
+                
 		    }		
 	    } 
 
