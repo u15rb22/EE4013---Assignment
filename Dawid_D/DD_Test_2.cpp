3cb4f86f0b1aaa712eb504cf49cd7828d62b774c
Dawid-Drewicz_UoA <u08dd22@abdn.ac.uk>
2025-12-04 22:48:49 +0000

    classes year, month, day working. Class Dates Functional with random generated items, enter from Keyboard to come...

---
+++ Dawid_D/DD_Test_2.cpp
@@ -0,0 +1,81 @@
+// EE4013 - Assignment 1
+// Release 2 - Test file 1 
+// by Dawid Drewicz 
+//
+// DD_Test_2.cpp
+
+#include "Dates_R2_DD.h"
+
+void test1_randGenIndiv()
+{
+    cout << "test1_randGenIndiv" << endl;
+    year_Item the_year; 
+
+    the_year.generateRandomItem(); 
+    the_year.printItemTypeName();
+    the_year.printItemOnScreen();
+
+    month_Item the_month; 
+
+    the_month.generateRandomItem(); 
+    the_month.printItemTypeName();
+    the_month.printItemOnScreen();
+
+    day_Item the_day;
+
+    the_day.generateRandomItem(); 
+    the_day.printItemTypeName();
+    the_day.printItemOnScreen();
+    
+
+}
+
+void test1_randGen()
+{
+    cout << "test1_randGen" << endl;
+    
+    dates_composite_Item Date;
+
+    Date.generateRandomItem(); 
+    Date.printItemTypeName();
+    Date.printItemOnScreen();
+
+}
+
+
+void test2_InputIndiv()
+{
+    cout << "test1_randGenIndiv" << endl;
+    year_Item the_year; 
+
+    the_year.printItemTypeName();
+    the_year.printItemOnScreen();
+    the_year.enterValueFromKeyboard();
+    the_year.printItemOnScreen();
+
+    month_Item the_month; 
+ 
+    the_month.printItemTypeName();
+    the_month.printItemOnScreen();
+    the_month.enterValueFromKeyboard();
+    the_month.printItemOnScreen();
+
+    day_Item the_day;
+     
+    the_day.printItemTypeName();
+    the_day.printItemOnScreen();
+    the_day.enterValueFromKeyboard();
+    the_day.printItemOnScreen();
+
+
+}
+
+int main()
+{
+    //test1_randGenIndiv();
+    //test2_InputIndiv();
+
+    test1_randGen();
+
+    return 0;
+}
--- Dawid_D/Dates_R2_DD.h
+++ Dawid_D/Dates_R2_DD.h
@@ -14,7 +14,8 @@ class year_Item: public integer_itemWithLimits{
         bool leap_year;
 
     private:
-        //using integer_itemWithLimits::generateRandomItem;
+        using integer_itemWithLimits::enterItemFromKeyboard;
+        using integer_itemWithLimits::enterLimitsFromKeyboard;
     public:
         year_Item()
         {
@@ -67,32 +68,36 @@ class year_Item: public integer_itemWithLimits{
 
         virtual void enterValueFromKeyboard()
         {
-            bool Valid_int = false;
-            while(!Valid_int)
+            if(isLocked())
+                cout << "Item is Locked." << endl;
+            else
             {
-		        cout << "Insert a Year then hit enter." << endl;
-		        cin >> item_value;
-                if(cin.fail())
+                bool Valid_int = false;
+                while(!Valid_int)
                 {
-                    cin.clear();
-                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-                    cout << "* Warning: Input must be an integer." << endl; 
-                    cout << endl;
-                }
-                else
-                {
-                    if(inLimits(item_value))
-                        Valid_int = true; 
+		            cout << "Insert a Year then hit enter." << endl;
+		            cin >> item_value;
+                    if(cin.fail())
+                    {
+                        cin.clear();
+                        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                        cout << "* Warning: Input must be an integer." << endl; 
+                        cout << endl;
+                    }
                     else
                     {
-                        cout << "*Warning*: year must fall within set limits :"<< max_val << "-" << min_val << endl << endl;  
+                        if(inLimits(item_value))
+                            Valid_int = true; 
+                        else
+                        {
+                            cout << "*Warning*: year must fall within set limits :"<< max_val << "-" << min_val << endl << endl;  
+                        } 
                     } 
-                } 
+                }
+
+                leap_year = is_leapYear(item_value);
+                empty = false;
             }
-
-            leap_year = is_leapYear(item_value);
-            empty = false;
-
         }
 
         virtual void generateRandomItem()// generates random Year
@@ -101,7 +106,7 @@ class year_Item: public integer_itemWithLimits{
                 cout << "Error in generateRandomitem: Item is Locked." << endl;
             else 
             {
-                generateRandomItem();
+                integer_itemWithLimits::generateRandomItem();
                 leap_year = is_leapYear(item_value);
             }
         }
@@ -115,10 +120,67 @@ class month_Item: public integer_itemWithLimits{
     protected:
         string month_text; 
 
-        virtual string PassMonthtext(int monthVal)
+        virtual string PassMonthText(int monthVal)
         {
+            string text; 
+
+            switch (monthVal)
+            {
+            case January:
+                text = "January";
+                break;
+            case February:
+                text = "February";
+                break;
+            case March:
+                text = "March";
+                break;
+            case April:
+                text = "April";
+                break;
+            case May:
+                text = "May";
+                break;
+            case June:
+                text = "June";
+                break;
+            case July:
+                text = "July";
+                break;
+            case August:
+                text = "August";
+                break;
+            case September:
+                text = "September";
+                break;
+            case October:
+                text = "October";
+                break;
+            case November:
+                text = "November";
+                break;
+            case December:
+                text = "December";
+                break;
             
+            default:
+                cout << "Error in PassMonthtext: value passed not in 1-12." << endl;
+                break;
+            }
+
+            return text; 
         }
+
+        virtual void SetMonth_text()
+        {
+            month_text = PassMonthText(item_value); 
+        }
+
+
+    private:
+        using integer_itemWithLimits::enterItemFromKeyboard;
+        using integer_itemWithLimits::enterLimitsFromKeyboard;
+
     public: 
         month_Item()
         {
@@ -128,11 +190,184 @@ class month_Item: public integer_itemWithLimits{
 
         ~month_Item(){;}
 
-        
+        virtual void printItemOnScreen() const 
+        {
+            if(isEmpty())
+                cout << "Item is empty." << endl; 
+            else
+            {
+                cout << " The Month is : "<< month_text ;
+                cout << ". Which is month number : " << item_value << endl;
+            }
+        }
+
+        virtual void generateRandomItem()// generates random Month
+        {
+            if(isLocked())
+                cout << "Error in generateRandomitem: Item is Locked." << endl;
+            else 
+            {
+                integer_itemWithLimits::generateRandomItem();
+                SetMonth_text();
+            }
+        }
+
+        virtual void enterValueFromKeyboard()
+        {
+            if(isLocked())
+                cout << "Item is Locked."<< endl;
+            else
+            {
+                bool Valid_int = false;
+                while(!Valid_int)
+                {
+		            cout << "Insert a Month in numerical format then hit enter." << endl;
+		            cin >> item_value;
+                    if(cin.fail())
+                    {
+                        cin.clear();
+                        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                        cout << "* Warning: Input must be an integer." << endl; 
+                        cout << endl;
+                    }
+                    else
+                    {
+                        if(inLimits(item_value))
+                            Valid_int = true; 
+                        else
+                        {
+                            cout << "*Warning*: there is 12 months, you must enter a value between :"<< min_val << "-" << max_val << endl << endl;  
+                        } 
+                    } 
+
+                }
+
+                SetMonth_text();
+                empty = false;
+            }
+        }
 
 };
 
-class day_Item: public integer_itemWithLimits{
+class day_Item: public integer_itemWithLimits{  // Scrap class, do inside Dates Class to avoid dublicate variables. 
+    protected:
+        int month_val;
+        bool inLeapYear; 
+
+        int NumbOfDaysIn(int month)
+        {
+            int NumOfDays; 
+
+            if((month ==March)||(month ==May)||(month ==July)||(month ==August)||(month ==October)||(month ==December))
+                NumOfDays = 31;
+            else if((month ==June)||(month ==September)||(month ==November))
+                NumOfDays = 30;
+            else 
+            {
+                if(inLeapYear)
+                    NumOfDays = 29;
+                else
+                    NumOfDays = 28;
+            }
+            return NumOfDays;
+        }
+
+        using integer_itemWithLimits::enterItemFromKeyboard;
+        using integer_itemWithLimits::enterLimitsFromKeyboard;
+
+        void setDays()
+        {
+            setLimits(1,NumbOfDaysIn(month_val));
+        }
+
+    public: 
+        day_Item()
+        {
+            itemTypeName = "day_item";
+            month_val = January; // set defult as 1
+            inLeapYear = false; // deafult not as not leap year 
+            setLimits(1,31);
+        }
+
+        ~day_Item(){;}
+
+        virtual void printItemOnScreen() const 
+        {
+            if(isEmpty())
+                cout << "Item is empty." << endl; 
+            else
+            {
+                cout << " The Day is : # "<< item_value ;
+                cout << ". in month number : " << month_val << " . Which has : "<< max_val; 
+                cout << " Days. and is "; 
+                if(inLeapYear)
+                    cout << "in a Leap Year." << endl;
+                else 
+                    cout << "in a Non-Leap year." << endl;
+            }
+        }
+
+        virtual void generateRandomItem()// generates random day within set month
+        {
+            if(isLocked())
+                cout << "Error in generateRandomitem: Item is Locked." << endl;
+            else 
+            {
+                integer_itemWithLimits::generateRandomItem();
+                setDays();
+            }
+        }
+
+        void generateRandomItem(int monthIn, bool LeapYearIn)
+        {
+            if(isLocked())
+                cout << "Error in generateRandomItem(int,bool): Item is Locked." << endl;
+            else
+            {
+                if((monthIn<=12)&&(monthIn>=1))
+                {
+                    month_val = monthIn; 
+                    inLeapYear = LeapYearIn;
+                    generateRandomItem();
+                }
+                else
+                    cout << "Error in generateRandomItem(int,bool): Invalid passed month value ." << endl;
+            }
+            
+        }
+
+        virtual void enterValueFromKeyboard()
+        {
+            if(isLocked())
+                cout << "Item is Locked."<< endl;
+            else
+            {
+                bool Valid_int = false;
+                while(!Valid_int)
+                {
+		            cout << "Insert number of Day then hit enter." << endl;
+		            cin >> item_value;
+                    if(cin.fail())
+                    {
+                        cin.clear();
+                        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                        cout << "* Warning: Input must be an integer." << endl; 
+                        cout << endl;
+                    }
+                    else
+                    {
+                        if(inLimits(item_value))
+                            Valid_int = true; 
+                        else
+                        {
+                            cout << "*Warning*: there is " << max_val << " days in chosen month, you must enter a value between :"<< min_val << "-" << max_val << endl << endl;  
+                        } 
+                    } 
+
+                }
+                empty = false;
+            }
+        }
 
 };
 
@@ -141,6 +376,66 @@ class dates_composite_Item: public basic_item{
         year_Item Year;
         month_Item Month;
         day_Item Day; 
+
+    public:
+        dates_composite_Item()
+        {
+            itemTypeName = "dates_composite_item";
+        }
+
+        ~dates_composite_Item(){;}
+
+        virtual void printItemOnScreen() const
+        {
+            if(isEmpty())
+                cout << "Item is Empty." << endl;
+            else
+            {
+                Year.printItemTypeName();
+                Year.printItemOnScreen();
+                Month.printItemTypeName();
+                Month.printItemOnScreen();
+                Day.printItemTypeName();
+                Day.printItemOnScreen();
+            }
+        }
+
+        virtual void generateRandomItem()
+        {
+            if(isLocked())
+                cout << "Error in generateRandomitem: Item is Locked." << endl;
+            else 
+            {
+                Year.generateRandomItem();
+                Month.generateRandomItem();
+                Day.generateRandomItem(Month.getItemVal(),Year.GetLeapStatues());
+                empty = false;
+            }
+        }
+
+
+        
+	virtual void enterItemFromKeyboard()
+    {
+        ;
+    }
+
+
+	virtual bool compatibilityCheck(const basic_item* other_item) const
+    {
+        ;
+    }
+	virtual bool IsLargerThan(const basic_item* other_item, const basic_sort_criteria* sort_criteria=NULL) const 
+    {
+        ;
+    }	
+	virtual bool IsEqualTo(const basic_item* other_item, const basic_sort_criteria* sort_criteria=NULL) const
+    {
+        ;
+    }	
+
+
+        
 };
 
 #endif
