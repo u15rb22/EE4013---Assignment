dd278da83c5c73c5287688c273b5bf713d645f4e
Ryan Burgess <u15rb22@abdn.ac.uk>
2025-12-18 22:38:09 +0000

    little changes to linked list that i forgot to push

--- Release4.h
+++ Release4.h
@@ -11,31 +11,42 @@ using namespace std;
 class linkedlist: public basic_item{
     private:
         //This creates a list of group1_items as container
-        list<group1_item> container;
+        list<group1_item*> container;
     public:
         linkedlist(){
             itemTypeName = "Linked_List_of_group1_items.";
         }
 
-        void insertAtHead(group1_item inp){
+        ~linkedlist(){
+            for(auto item : container){delete item;}
+            container.clear();
+        }
+
+        void insertAtHead(group1_item* inp){
             //Create a new node
             container.push_front(inp);
         }
 
-        void insertAtTail(group1_item inp){
+        void insertAtTail(group1_item* inp){
             //Create a new node
             container.push_back(inp);
         }
 
         void removeFromHead(){
             if(!container.empty()){
+                group1_item* Item = container.front();
                 container.pop_front();
+                //Deletes the node at the back and frees memory
+                delete Item;  
             }
         }
 
         void removeFromTail(){
             if(!container.empty()){
+                group1_item* Item = container.back();
                 container.pop_back();
+                //Deletes the node at the back and frees memory
+                delete Item; 
             }
         }
 
@@ -50,33 +61,39 @@ class linkedlist: public basic_item{
             //Const group1_item &item creates a read only reference 
             //To each of the group1_items in the container
             // : container will iterate through all the group1_items in the list
-            for(const group1_item &item : container) {
-                item.printItemOnScreen();
+            for(const group1_item* item : container) {
+                item->printItemOnScreen();
             }
         }
 
         virtual void enterItemFromKeyboard() override{
-            group1_item newItem;
-            cout << "Enter new item: " << endl;
-            newItem.enterItemFromKeyboard();
+            int size;
+            cout << "How many items to add to the list? " << endl;
+            cout << "Enter a number: " << endl;
+            cin >> size;
 
-            char choice;
-            cout << "Insert at the head or tail of the list: "<< endl;
-            cout << "Type H for head or T for tails:" << endl;
-            cin >> choice;
+            for(int i = 0; i < size; i++){
+                group1_item* newItem = new group1_item();
+                cout << "Enter new item: " << endl;
+                newItem->enterItemFromKeyboard();
 
-            if(choice == 'H' || choice == 'h'){
-                insertAtHead(newItem);
-            }
-            else{
-                insertAtTail(newItem);
+                char choice;
+                cout << "Insert at the head or tail of the list: "<< endl;
+                cout << "Type H for head or T for tails:" << endl;
+                cin >> choice;
+
+                if(choice == 'H' || choice == 'h'){
+                    insertAtHead(newItem);
+                }
+                else{
+                    insertAtTail(newItem);
+                }
             }
         }
 
         virtual void generateRandomItem() override {
-            group1_item newItem;
-
-            newItem.generateRandomItem();
+            group1_item* newItem = new group1_item();
+            newItem->generateRandomItem();
 
             char choice;
             cout << "Insert at the head or tail of the list: "<< endl;
@@ -92,21 +109,7 @@ class linkedlist: public basic_item{
         }
 
         virtual bool compatibilityCheck(const basic_item* other_item) const override{
-            //True if the same, false if not
-            bool result = false;
-
-            //Check to see if the other item is allocated, if not skip
-            if(other_item != NULL){
-                //Type casting the other item to confirm its the same as the string
-                const linkedlist* typecast_OtherItem = typecastItem(other_item, this);
-
-                if(typecast_OtherItem != NULL){result = true;}
-                else{
-                    cout << "Check failed for Item type: " << itemTypeName << endl;
-                }
-            }
-
-            return result;
+            return false;
         }
 
         virtual bool IsLargerThan(const basic_item* other_item, const basic_sort_criteria* sort_criteria=NULL) const override{
